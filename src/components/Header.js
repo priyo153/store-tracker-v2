01cc8ecaf8e18dfa69e75ff5080d1b3d4770bb72
priyo153 <priyojit.chatterjee333@gmail.com>
2020-06-29 01:40:13 +0530

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
     <React.Fragment>
       <nav className="header navbar navbar-dark bg-dark">
         <Link className="navbar-brand mb-0" to="/">
+          <img
+            alt="logo"
+            src="https://img.icons8.com/fluent/96/000000/buy.png"
+            className="mr-2 mb-2"
+            style={{ height: "2rem" }}
+          />
           Store Tracker
         </Link>
       </nav>
